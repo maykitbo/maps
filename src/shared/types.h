1b4561aedf6bb2f8ed1072b7142f62a2f2d14312
maykitbo <zam7@icloud.com>
2024-09-05 16:47:01 +0700

    bbox & point constructors

--- src/shared/types.h
+++ src/shared/types.h
@@ -9,19 +9,21 @@ namespace maykitbo::maps
 
 
 // float pointing type for coordinates
-typedef double coord_t
+typedef double coord_t;
 
 // float pointing type for route length
-typedef double distance_t
+typedef double distance_t;
 
 // integer type for nodes id
-typedef long long unsigned nodeid_t
+typedef long long unsigned nodeid_t;
 
 // struct for point on a map
 struct point_s
 {
     coord_t lat;
     coord_t lon;
+    point_s(coord_t lat, coord_t lon)
+        : lat(lat), lon(lon) {}
 };
 
 // struct for bbox. Rect on a map
@@ -29,13 +31,17 @@ struct bbox_s
 {
     point_s left_top;
     point_s right_buttom;
+    bbox_s(coord_t left, coord_t top, coord_t right, coord_t buttom)
+        : left_top(top, left), right_buttom(buttom, right) {}
+    bbox_s(const point_s& left_top, const point_s& right_buttom)
+        : left_top(left_top), right_buttom(right_buttom) {}
 };
 
 // storage type for route
 struct route_s
 {
     // route distance
-    ditance_t len;
+    distance_t len;
     // nodes to connect the route
     std::vector<nodeid_t> node_ids;
     // ways to connect the nodes
